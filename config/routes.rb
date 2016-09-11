30a0259f65ccc680c6a9e7f3b4714fa00835c2af
Szymon Blaszczynski <museyoucoulduse@icloud.com>
2016-09-10 19:10:55 -0700

    Unstable Beta release

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :pos_comments
-  resources :neg_comments
-  resources :positives
-  resources :negatives
+  
+  
+  resources :positives, except: [:edit, :destroy] do
+    resources :pos_comments, except: [:edit, :destroy]
+  end
+  resources :negatives, except: [:edit, :destroy] do
+    resources :neg_comments, except: [:edit, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
